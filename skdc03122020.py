987163159b339e9e2f591c58322b035fa150de48
Nishant Srinivas <nishantredmi99@gmail.com>
2020-12-03 07:40:07 +0530

    Create skdc03122020.py
    Input:
        4 4
        58 82 52 52
        25 44 36 89
        16 62 88 37
        90 63 13 30
        3 2
        Output:
        3276 1116 88 1066

---
+++ skdc03122020.py
@@ -0,0 +1,19 @@
+def GCD(a,b):
+    if(a==0):
+        return b
+    return GCD(b%a,a)
+def LCM(a,b):
+    return((a*b)//GCD(a,b))
+R,C = map(int,input().strip().split())
+mat = []
+for i in range(R):
+    mat.append(list(map(int,input().strip().split())))
+X,Y = map(int,input().strip().split())
+s = 0
+t = R-1
+for x in range(R):
+    #print(mat[s][X-1],mat[t][Y-1])
+    print(LCM(mat[s][X-1],mat[t][Y-1]),end = " ")
+    s+=1
+    t-=1
+    
