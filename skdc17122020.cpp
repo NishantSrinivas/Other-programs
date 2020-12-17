ba83159a76b2c644d734fea86e9bf6cdbf3b9a85
Nishant Srinivas <nishantredmi99@gmail.com>
2020-12-17 20:44:44 +0530

    Create skdc17122020.cpp

---
+++ skdc17122020.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1 {},s2 {};
+    getline(cin,s1);
+    getline(cin,s2);
+    int s1_size {s1.size()},start_idx {0},end_idx {s1_size-1};
+    for(in t i=0;i<s1_size;i++){
+        if(s1.at(i)!=s2.at(i)){
+            break;
+        } 
+        else{
+            start_idx++;
+        }
+    }
+    for(int j=s1_size-1;j>0;j--){
+        if(s1.at(j)!=s2.at(j)){
+            break;
+        }
+        else{
+            end_idx--;
+        }
+    }
+    cout<<s1.substr(start_idx,(end_idx-start_idx)+1)<<"\n";
+    return 0;
+}
